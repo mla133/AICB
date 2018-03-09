206476080762384ed4e81a1ff8831150e62d822c
Matt Allen <matthew.allen@technipfmc.com>
2018-03-09 12:54:46 -0500

    Updating inj_addr and parameter array to be more flexible.

--- aicb.py
+++ aicb.py
@@ -24,21 +24,16 @@ PC_cmd = 'PC'
 SO_cmd = 'SO'
 RC_cmd = 'RC'
 
-NRT1 = NRT2 = NRT3 = NRT4 = 0
-PC1 = PC2 = PC3 = PC4 = 0
 OS_P1 = OS_P2 = OS_P3 = OS_P4 = 0
 OS_S1 = OS_S2 = OS_S3 = OS_S4 = 0
-k_factor = 5000.0
-mtr_factor = 1.0
-conv_factor = 3785.412
 alrm_pulse_count = alrm_pulse_time = 0
-inj_vol = 25.0
-control_method = 2
+inj_addr = 0
+#         0   ,      1,   2,   3,   4, 5,        6, 7, 8, 9,10,11,12,   13,  14,15
 inj =  [
-        ['101', 5000.0, 1.0, 0.0, 0.0, 0, 3785.412, 0, 0, 2, 0, 0, 0, 25.0],
-        ['102', 5000.0, 1.0, 0.0, 0.0, 0, 3785.412, 0, 0, 2, 0, 0, 0, 25.0],
-        ['103', 5000.0, 1.0, 0.0, 0.0, 0, 3785.412, 0, 0, 2, 0, 0, 0, 25.0],
-        ['104', 5000.0, 1.0, 0.0, 0.0, 0, 3785.412, 0, 0, 2, 0, 0, 0, 25.0],
+        ['301', 5000.0, 1.0, 0.0, 0.0, 0, 3785.412, 0, 0, 2, 0, 0, 0, 25.0, 0.0, 0],
+        ['302', 5000.0, 1.0, 0.0, 0.0, 0, 3785.412, 0, 0, 2, 0, 0, 0, 25.0, 0.0, 0],
+        ['303', 5000.0, 1.0, 0.0, 0.0, 0, 3785.412, 0, 0, 2, 0, 0, 0, 25.0, 0.0, 0],
+        ['304', 5000.0, 1.0, 0.0, 0.0, 0, 3785.412, 0, 0, 2, 0, 0, 0, 25.0, 0.0, 0],
        ]
  
 while True:
@@ -50,48 +45,67 @@ while True:
                 command = line[3:5]
                 param = line[6:8]
                 value = line[8:len(line)]
-
+                inj_addr = (int)(addr)-301
 
 		if (command == IN_cmd):
+                        # NRT (14) += INJ_VOL (13) / CONV_FACT (6)
+                        # PC (15) += NRT (14) * K_FACT (1)
 			if addr == '301':
-                                NRT1 += inj_vol / conv_factor
-				PC1 += NRT1 * k_factor 
-			elif addr == '302':
-				NRT2 += 0.026
-				PC2 += 133
-			elif addr == '303':
-				NRT3 += 0.026
-				PC3 += 133
-			elif addr == '304':
-				NRT4 += 0.026
-				PC4 += 133
+                                inj[inj_addr][14] += inj[inj_addr][13] / inj[inj_addr][6]
+				inj[inj_addr][15] += inj[inj_addr][14] * inj[inj_addr][1] 
 			resp = "OK"
 
 		elif (command == EP_cmd or command == DP_cmd):
-			resp = "OK"
+                        resp = "OK"
 
                 elif (command == OS_cmd):
-                        if param == 'P':
-                            resp = "OS P %d" % OS_P1 
-                        if param == 'S':
-                            resp = "OS S %d" % OS_S1 
+                        if addr == '301':
+                            if param == 'P':
+                                resp = "OS P %d" % OS_P1 
+                            if param == 'S':
+                                resp = "OS S %d" % OS_S1 
+                        if addr == '302':
+                            if param == 'P':
+                                resp = "OS P %d" % OS_P2 
+                            if param == 'S':
+                                resp = "OS S %d" % OS_S2 
+                        if addr == '303':
+                            if param == 'P':
+                                resp = "OS P %d" % OS_P3 
+                            if param == 'S':
+                                resp = "OS S %d" % OS_S3 
+                        if addr == '304':
+                            if param == 'P':
+                                resp = "OS P %d" % OS_P4 
+                            if param == 'S':
+                                resp = "OS S %d" % OS_S4 
 
                 elif (command == SO_cmd):
-                        if param == 'P':
-                            OS_P1 = int(value)
-                        if param == 'S':
-                            OS_S1 = int(value)
+                        if addr == '301':
+                            if param == 'P':
+                                OS_P1 = int(value)
+                            if param == 'S':
+                                OS_S1 = int(value)
+                        if addr == '302':
+                            if param == 'P':
+                                OS_P2 = int(value)
+                            if param == 'S':
+                                OS_S2 = int(value)
+                        if addr == '303':
+                            if param == 'P':
+                                OS_P3 = int(value)
+                            if param == 'S':
+                                OS_S3 = int(value)
+                        if addr == '304':
+                            if param == 'P':
+                                OS_P4 = int(value)
+                            if param == 'S':
+                                OS_S4 = int(value)
+
                         resp = "OK"
 
 		elif (command == TS_cmd):
-			if addr == '301':
-				resp = "TS %12.3f 0000" % NRT1
-			if addr == '302':
-				resp = "TS %12.3f 0000" % NRT2
-			if addr == '303':
-				resp = "TS %12.3f 0000" % NRT3
-			if addr == '304':
-				resp = "TS %12.3f 0000" % NRT4
+                        resp = "TS %12.3f 0000" % (float)(inj[(int)(addr)-301][14])
 
 		elif (command == ST_cmd):
 			resp = "ST 0000"
@@ -104,28 +118,30 @@ while True:
                             resp = "OK"
 
                 elif (command == PW_cmd):
-                        if param == '10':
-                            k_factor = float(value)
-                        if param == '11':
-                            mtr_factor = float(value)
-                        if param == '20':
-                            hi_tol = float(value)
-                        if param == '20':
-                            lo_tol = float(value)
-                        if param == '23':
-                            conv_fact = float(value)
-                        if param == '24':
-                            alrm_pulse_count = int(value)
-                        if param == '25':
-                            alrm_pulse_time = int(value)
-                        if param == '26':
-                            control_method = int(value)
-                        if param == '30':
-                            inj_vol = float(value)
+                        if addr == '301':
+                            if param == '10':
+                                inj[0][1] = float(value)
+                            if param == '11':
+                                inj[0][2] = float(value)
+                            if param == '20':
+                                hi_tol = float(value)
+                            if param == '20':
+                                lo_tol = float(value)
+                            if param == '23':
+                                conv_fact = float(value)
+                            if param == '24':
+                                alrm_pulse_count = int(value)
+                            if param == '25':
+                                alrm_pulse_time = int(value)
+                            if param == '26':
+                                control_method = int(value)
+                            if param == '30':
+                                inj_vol = float(value)
                         resp = "OK" 
 
                 elif (command == PC_cmd):
                         resp = "PC %06d" % PC1
+
                 elif (command == RC_cmd):
                         PC1 = 0
                         resp = "OK"
